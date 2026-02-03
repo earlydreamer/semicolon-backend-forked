c4ad2aaeac35ccc0b1a3930ed747ebe18e722789
ooooo_ori <dhtp0112@gmail.com>
2026-02-03 12:19:44 +0900

    Feat(product-comment): 댓글 생성/대댓글/수정/삭제/목록 조회 유즈케이스 구현

---
+++ semicolon/src/main/java/dukku/semicolon/boundedContext/product/app/usecase/comment/UpdateCommentUseCase.java
@@ -0,0 +1,47 @@
+package dukku.semicolon.boundedContext.product.app.usecase.comment;
+
+import dukku.semicolon.boundedContext.product.entity.Product;
+import dukku.semicolon.boundedContext.product.entity.ProductComment;
+import dukku.semicolon.boundedContext.product.out.ProductCommentRepository;
+import dukku.semicolon.shared.product.dto.comment.CommentResponse;
+import dukku.semicolon.shared.product.dto.comment.CommentUpdateRequest;
+import dukku.semicolon.shared.product.exception.CommentNotFoundException;
+import dukku.semicolon.shared.product.exception.CommentUnauthorizedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class UpdateCommentUseCase {
+
+    private final ProductCommentRepository commentRepository;
+
+    @Transactional
+    public CommentResponse execute(UUID userUuid, UUID productUuid, UUID commentUuid, CommentUpdateRequest request) {
+
+        ProductComment comment = commentRepository.findByUuid(commentUuid)
+                .orElseThrow(CommentNotFoundException::new);
+
+        Product product = comment.getProduct();
+
+        if(comment.isDeleted()) throw  new CommentUnauthorizedException();
+
+        // (1) 상품 소속 검증
+        if (!product.getUuid().equals(productUuid)) {
+            throw new CommentNotFoundException();
+        }
+
+        // (2) 작성자 검증
+        if (!comment.getAuthorUuid().equals(userUuid)) {
+            throw new CommentUnauthorizedException();
+        }
+
+        // (3) 수정
+        comment.changeContent(request.getContent());
+
+        return CommentResponse.from(comment);
+    }
+}
