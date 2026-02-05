d4feeca27f6c0e9a5f8267c91984ffcbed6172bb
ooooo_ori <dhtp0112@gmail.com>
2026-02-05 10:18:46 +0900

    Refactor(review-stats): 리뷰 통계 동기화 시 seller 일괄 조회 및 saveAll로 처리 구조 변경

--- semicolon/src/main/java/dukku/semicolon/boundedContext/product/entity/ProductSeller.java
+++ semicolon/src/main/java/dukku/semicolon/boundedContext/product/entity/ProductSeller.java
@@ -72,4 +72,9 @@ public class ProductSeller extends BaseIdAndUUIDAndTime {
     public void changeIntro(String intro) {
         this.intro = intro;
     }
+
+    public void updateReviewSummary(int reviewCount, BigDecimal averageRating) {
+        this.reviewCount = reviewCount;
+        this.averageRating = averageRating;
+    }
 }
--- semicolon/src/main/java/dukku/semicolon/boundedContext/product/out/ProductSellerRepository.java
+++ semicolon/src/main/java/dukku/semicolon/boundedContext/product/out/ProductSellerRepository.java
@@ -2,10 +2,9 @@ package dukku.semicolon.boundedContext.product.out;
 
 import dukku.semicolon.boundedContext.product.entity.ProductSeller;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
-import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,14 +13,5 @@ public interface ProductSellerRepository extends JpaRepository<ProductSeller, In
 
     Optional<ProductSeller> findByUuid(UUID shopUuid);
 
-    Optional<ProductSeller> findBySellerUuid(UUID sellerUuid);
-
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("""
-        update ProductSeller ps
-           set ps.reviewCount = :reviewCount,
-               ps.averageRating = :averageRating
-         where ps.sellerUuid = :sellerUuid
-    """)
-    int updateReviewSummary(UUID sellerUuid, int reviewCount, BigDecimal averageRating);
+    List<ProductSeller> findBySellerUuidIn(Collection<UUID> sellerUuids);
 }
