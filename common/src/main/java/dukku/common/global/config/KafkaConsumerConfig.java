d2e178e74c10492aa777f532389a918dcf00abbd
Yoo-SH <ads137485@jbnu.ac.kr>
2026-02-10 12:27:16 +0900

    fix(#142):어노테이션  @ConditionalOnBean(ConsumerFactory.class) 추가

--- common/src/main/java/dukku/common/global/config/KafkaConsumerConfig.java
+++ common/src/main/java/dukku/common/global/config/KafkaConsumerConfig.java
@@ -1,6 +1,7 @@
 package dukku.common.global.config;
 
 import dukku.common.global.logging.KafkaTracingRecordInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,17 +14,22 @@ import org.springframework.kafka.core.ConsumerFactory;
  * <p>Spring Kafka의 {@code ConcurrentKafkaListenerContainerFactory}에
  * {@code KafkaTracingRecordInterceptor}를 등록하여
  * 레코드 단위 MDC 라이프사이클을 보장합니다.</p>
+ *
+ * <p>테스트 환경 등 Kafka 자동 설정이 비활성화된 경우
+ * {@code ConsumerFactory} 빈이 없으므로 팩토리 생성을 건너뜁니다.</p>
  */
 @Configuration
 @ConditionalOnClass(ConcurrentKafkaListenerContainerFactory.class)
 public class KafkaConsumerConfig {
 
     @Bean
+    @ConditionalOnBean(ConsumerFactory.class)
     public KafkaTracingRecordInterceptor kafkaTracingRecordInterceptor() {
         return new KafkaTracingRecordInterceptor();
     }
 
     @Bean
+    @ConditionalOnBean(ConsumerFactory.class)
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
             ConsumerFactory<String, Object> consumerFactory,
             KafkaTracingRecordInterceptor kafkaTracingRecordInterceptor) {
