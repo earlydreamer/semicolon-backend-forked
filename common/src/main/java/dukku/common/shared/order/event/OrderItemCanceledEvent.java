d0d2c585cd8f599149e39b87959358df62dd72c3
HwangSlater <doorchan23@naver.com>
2026-02-11 20:06:35 +0900

    Refactor: deploy.yml 수정

--- common/src/main/java/dukku/common/shared/order/event/OrderItemCanceledEvent.java
+++ common/src/main/java/dukku/common/shared/order/event/OrderItemCanceledEvent.java
@@ -1,6 +1,17 @@
 package dukku.common.shared.order.event;
 
+import dukku.common.global.event.DomainEvent;
+
 import java.util.UUID;
 
-public record OrderItemCanceledEvent(UUID orderItemUuid) {
+public record OrderItemCanceledEvent(UUID orderItemUuid) implements DomainEvent {
+    @Override
+    public String getTopic() {
+        return "order.item.canceled";
+    }
+
+    @Override
+    public String getKey() {
+        return orderItemUuid.toString();
+    }
 }
