24fee16b91e305b4e881e340d776d251098252d4
HwangSlater <doorchan23@naver.com>
2026-02-11 20:06:35 +0900

    Refactor: deploy.yml 수정

--- common/src/main/java/dukku/common/shared/order/event/OrderItemRefundRequestedEvent.java
+++ common/src/main/java/dukku/common/shared/order/event/OrderItemRefundRequestedEvent.java
@@ -1,6 +1,17 @@
 package dukku.common.shared.order.event;
 
+import dukku.common.global.event.DomainEvent;
+
 import java.util.UUID;
 
-public record OrderItemRefundRequestedEvent(UUID orderItemUuid) {
+public record OrderItemRefundRequestedEvent(UUID orderItemUuid) implements DomainEvent {
+    @Override
+    public String getTopic() {
+        return "order.item.refund-requested";
+    }
+
+    @Override
+    public String getKey() {
+        return orderItemUuid.toString();
+    }
 }
