e56137a8eb79f3c08912cf3d3077b8443d633393
HwangSlater <doorchan23@naver.com>
2026-02-12 13:27:12 +0900

    Refactor: deploy.yml 수정

--- common/src/main/java/dukku/common/global/config/GlobalConfig.java
+++ common/src/main/java/dukku/common/global/config/GlobalConfig.java
@@ -15,10 +15,4 @@ public class GlobalConfig {
         GlobalConfig.eventPublisher = eventPublisher;
     }
 
-    @org.springframework.context.annotation.Bean
-    public com.fasterxml.jackson.databind.ObjectMapper objectMapper() {
-        return new com.fasterxml.jackson.databind.ObjectMapper()
-                .registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule())
-                .disable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    }
 }
