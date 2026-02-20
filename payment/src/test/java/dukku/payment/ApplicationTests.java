f16a35718df3cb097e23c06d51d44f91d5fae3f4
Jaehyun Park <earlydreamer@users.noreply.github.com>
2026-02-20 16:39:15 +0900

    Chore(test): Application 기동 테스트에 하드코딩된 환경변수 주입 제거
    - 하드코딩 제거 (application-dev를 경유해 띄우도록 수정)

--- payment/src/test/java/dukku/payment/ApplicationTests.java
+++ payment/src/test/java/dukku/payment/ApplicationTests.java
@@ -2,25 +2,8 @@ package dukku.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "spring.jpa.properties.hibernate.hbm2ddl.auto=create-drop",
-        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
-        "spring.kafka.listener.auto-startup=false",
-        "spring.kafka.bootstrap-servers=localhost:9092",
-        "spring.data.redis.host=localhost",
-        "spring.data.redis.port=6379",
-        "spring.elasticsearch.uris=http://localhost:9200",
-        "jwt.access.secret.key=dGhpcy1rZXktaXMtdGVzdC1rZXktYWNjZXNzLTAxMjM=",
-        "jwt.refresh.secret.key=dGhpcy1rZXktaXMtdGVzdC1rZXktcmVmcmVzaC0wMTI=",
-        "crypto.key=dGhpcy1rZXktaXMtdGVzdC1rZXktY3J5cHRvLTAxMjM=",
-        "toss.api.secret-key=test_toss_key"
-})
+@SpringBootTest
 class ApplicationTests {
 
     @Test
