fe967f677c049d62f44cd23c5ea626f8a6c336ec
ooooo_ori <dhtp0112@gmail.com>
2026-02-21 00:57:00 +0900

    Chore: 충돌 해결 과정에서 누락된 미사용 CustomException 삭제

--- common/src/main/java/dukku/common/shared/product/exception/CannotAddOwnProductException.java
+++
@@ -1,9 +0,0 @@
-package dukku.common.shared.product.exception;
-
-import dukku.common.global.exception.BadRequestException;
-
-public class CannotAddOwnProductException extends BadRequestException {
-    public CannotAddOwnProductException() {
-        super("자신의 상품은 장바구니에 담을 수 없습니다.");
-    }
-}
--- common/src/main/java/dukku/common/shared/product/exception/CartAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package dukku.common.shared.product.exception;
-
-import dukku.common.global.exception.ConflictException;
-
-public class CartAlreadyExistsException extends ConflictException {
-    public CartAlreadyExistsException() {
-        super("이미 장바구니에 담긴 상품입니다.");
-    }
-}
