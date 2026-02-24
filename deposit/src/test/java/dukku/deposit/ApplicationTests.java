3cf4a5eea8f819dea82126b9833c58c343a00b89
ooooo_ori <dhtp0112@gmail.com>
2026-02-24 10:25:11 +0900

    Fix(build) : ci Build 해결

--- deposit/src/test/java/dukku/deposit/ApplicationTests.java
+++ deposit/src/test/java/dukku/deposit/ApplicationTests.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.kafka.listener.auto-startup=false",
+        "spring.kafka.bootstrap-servers=localhost:19092"
+})
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Test
--- order/src/test/java/dukku/order/ApplicationTests.java
+++ order/src/test/java/dukku/order/ApplicationTests.java
@@ -2,8 +2,13 @@ package dukku.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.kafka.listener.auto-startup=false",
+        "spring.kafka.bootstrap-servers=localhost:19092"
+})
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Test
